e14b992fff69d71e2e4c4141e5452b67a2625f11
kerrlabajo <kerr.labajo@cit.edu>
2023-07-22 20:38:14 +0800

    Ignore and untrack items

--- src/models/InputFile.py
+++
@@ -1,6 +0,0 @@
-class InputFile:
-    def __init__(self, dimensions, size, extension, filename):
-        self.dimensions = dimensions
-        self.size = size
-        self.extension = extension
-        self.filename = filename
--- src/models/OutputFile.py
+++
@@ -1,6 +0,0 @@
-class OutputFile:
-    def __init__(self, classification, accuracy, error_rate, filename):
-        self.classification = classification
-        self.accuracy = accuracy
-        self.error_rate = error_rate
-        self.filename = filename
