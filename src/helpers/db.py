6c690768eb21776d79684c996f31acc2bee126e3
kerrlabajo <kerr.labajo@cit.edu>
2023-08-02 00:28:04 +0800

    Update document attributes for posting input and output files

--- src/helpers/db.py
+++ src/helpers/db.py
@@ -1,18 +1,25 @@
+from datetime import datetime
 from pymongo import MongoClient
 from config import MONGO_URI, MONGO_DB, MONGO_COLLECTION_WEIGHTS, MONGO_COLLECTION_INPUT_FILES, MONGO_COLLECTION_OUTPUT_FILES
 from bson.binary import Binary
 import io
 from PIL import Image
-import torch
+from flask import session
 
 def insert_input_file(input_image_path):
     mongo = MongoClient(MONGO_URI)[MONGO_DB]  # Connect to MongoDB using the URI and database name
     with open(input_image_path, "rb") as image_file:
         input_image = image_file.read()
     
-    last_slash_index = image_file.name.rfind("\\")
-    filtered_name = image_file.name[last_slash_index + 1:]
-    image_document = {"name": filtered_name, "data": Binary(input_image)}
+    current_input_file = session.get('input_file')
+    
+    image_document = {
+        "name": current_input_file['filename'], 
+        "dimensions": current_input_file['dimensions'], 
+        "size": current_input_file['size'], 
+        "extension": current_input_file['extension'], 
+        "data": Binary(input_image)
+    }
     mongo[MONGO_COLLECTION_INPUT_FILES].insert_one(image_document)
     
 def get_input_file(image_name):
@@ -35,9 +42,15 @@ def insert_output_file(predicted_image_path):
     with open(predicted_image_path, "rb") as image_file:
         predicted_image = image_file.read()
     
-    last_slash_index = image_file.name.rfind("/")
-    filtered_name = image_file.name[last_slash_index + 1:]
-    image_document = {"name": filtered_name, "data": Binary(predicted_image)}
+    current_output_file = session.get('output_file')
+    
+    image_document = {
+        "name": current_output_file['filename'],
+        "classification": current_output_file['classification'],
+        "accuracy": current_output_file['accuracy'],
+        "error_rate": current_output_file['error_rate'],
+        "data": Binary(predicted_image)
+    }
     mongo[MONGO_COLLECTION_OUTPUT_FILES].insert_one(image_document)
 
 # This method was for testing purposes to decode the binary data and save/display as an image.
@@ -58,16 +71,24 @@ def get_output_file(image_name):
     
 def insert_weights(pt_file_path, model_name):
     mongo = MongoClient(MONGO_URI)[MONGO_DB]
-
+    
+    # best.pt file is too large (22mb) to be stored in MongoDB (16mb maximum), so we will store its path instead
     # Read the PyTorch model file
-    with open(pt_file_path, 'rb') as pt_file:
-        pt_model_data = pt_file.read()
+    # with open(pt_file_path, 'rb') as pt_file:
+    #     pt_model_data = pt_file.read()
 
     # Convert the model data to BSON binary format
-    bson_model_data = Binary(pt_model_data)
+    # bson_model_data = Binary(pt_model_data)
 
     # Create a document for the model and insert it into the MongoDB collection
-    model_document = {"name": model_name, "data": bson_model_data}
+    now = datetime.now()
+    date_time_str = now.strftime('%Y-%m-%d_%H-%M')
+    
+    model_document = {
+        "name": model_name, 
+        "date_generated": date_time_str,
+        "path": pt_file_path
+    }
     mongo[MONGO_COLLECTION_WEIGHTS].insert_one(model_document)
 
 def get_weights(model_name):
@@ -75,21 +96,6 @@ def get_weights(model_name):
 
     # Find the model document by name
     model_document = mongo[MONGO_COLLECTION_WEIGHTS].find_one({"name": model_name})
-
-    if model_document is None:
-        raise ValueError(f"Model '{model_name}' not found in the database.")
-
-    # Retrieve the model data from the document
-    bson_model_data = model_document["data"]
-
-    # Convert the BSON binary data back to bytes
-    pt_model_data = bson_model_data
-
-    # Load the PyTorch model from the bytes data
-    model = torch.load(io.BytesIO(pt_model_data))
-
-    return model
-
-# Example usage:
-# insert_pt_model('/path/to/your/model.pt', 'MyPyTorchModel')
-# loaded_model = load_pt_model('MyPyTorchModel')
+    
+    # Return the model document
+    return model_document
