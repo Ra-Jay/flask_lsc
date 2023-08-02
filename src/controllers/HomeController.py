34f586ec61ccab8ebdac0bc7ab8886c1251ebc92
kerrlabajo <kerr.labajo@cit.edu>
2023-08-03 02:34:32 +0800

    Modify HomeController
    Modifications:
    1. Import io and PIL Image.
    2. Create condition for handling existing input/output files.
    3. Add folder handlers for both.
    4, Initialize these existing input/output files to their corresponding models to send as blueprint when rendering template index.html.
    5. Handle saving of these existing files.
    6. Add print messages.

--- src/controllers/HomeController.py
+++ src/controllers/HomeController.py
@@ -1,5 +1,6 @@
 import torch
 import os
+import io
 from models.InputFile import InputFile
 from models.OutputFile import OutputFile
 from flask import Blueprint, render_template, request, redirect, url_for, current_app, send_from_directory
@@ -9,6 +10,7 @@ from datetime import datetime
 from helpers.Utility import analyze_image, get_file_dimensions, get_file_size, get_file_extension, allowed_file
 from flask import session
 from time import time
+from PIL import Image
 
 input_file = Blueprint('input_file', __name__)
 output_file = Blueprint('output_file', __name__)
@@ -65,10 +67,24 @@ def upload():
             if existing_input_file['dimensions'] != dimensions or existing_input_file['size'] != size or existing_input_file['extension'] != extension:
                 insert_input_file(path)
             else:
-                input_file = existing_input_file
+                # Create a folder if the upload folder doesn't exist and store the image there
+                if not os.path.exists(app.config['UPLOAD_FOLDER']):
+                    os.makedirs(app.config['UPLOAD_FOLDER'])
+                
+                input_file = InputFile(existing_input_file['dimensions'], existing_input_file['size'], existing_input_file['extension'], existing_input_file['name'])
+                stream = io.BytesIO(existing_input_file['data'])
+                input_image = Image.open(stream)
+                input_image.save(app.config['UPLOAD_FOLDER'] + '/' + existing_input_file['name'])
+                
+                elapsed_time = time() - start_time  # Calculate elapsed time
+                print(f"Existing image from mongodb saved to {app.config['UPLOAD_FOLDER']}/{existing_input_file['name']} in {elapsed_time:.2f} seconds")
+                print("===============================")
+                
+                return render_template('index.html', input_file=input_file)
+                
         
         elapsed_time = time() - start_time  # Calculate elapsed time
-        print(f"Input file object created and inserted in {elapsed_time:.2f} seconds")
+        print(f"Input file object created and inserted to mongodb in {elapsed_time:.2f} seconds")
         print("===============================")
         
         return render_template('index.html', input_file=input_file)
@@ -77,6 +93,7 @@ def upload():
 
 @input_file.route('/analyze', methods=['GET', 'POST'])
 def analyze():
+    from app import app
     if request.method == 'POST':
         input_file_data = session.get('input_file')
 
@@ -133,10 +150,24 @@ def analyze():
             if existing_output_file['classification'] != classification or existing_output_file['accuracy'] != accuracy or existing_output_file['error_rate'] != error_rate:
                 insert_output_file(path)
             else:
-                output_file = existing_output_file
+                older_date = existing_output_file['created_at'].replace(':', '-').replace(' ', '_')[:-3]
+                # Create a folder for the older date if it doesn't exist and store the image there
+                if not os.path.exists(app.config['PREDICTIONS_FOLDER'] + "/" +  older_date):
+                    os.makedirs(app.config['PREDICTIONS_FOLDER'] + "/" +  older_date)
+                    
+                older_path = app.config['PREDICTIONS_FOLDER'] + "/" + older_date + '/' + existing_output_file['name']
+                output_file = OutputFile(existing_output_file['classification'], existing_output_file['accuracy'], existing_output_file['error_rate'], older_path, existing_output_file['name'])
+                stream = io.BytesIO(existing_output_file['data'])
+                output_image = Image.open(stream)
+                output_image.save(older_path)
+                elapsed_time = time() - start_time
+                print(f"Existing image from mongodb saved to {older_path} in {elapsed_time:.2f} seconds")
+                print("===============================")
+                
+                return render_template('index.html', input_file=input_file_data, output_file=output_file)
         
         elapsed_time = time() - start_time
-        print(f"Output file object created and inserted in {elapsed_time:.2f} seconds")
+        print(f"Output file object created and inserted to mongodb in {elapsed_time:.2f} seconds")
         print("===============================")
         
         return render_template('index.html', input_file=input_file_data, output_file=output_file)
