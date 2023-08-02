0b7367f0d1414577f181927a3cea6cbff690f5fc
kerrlabajo <kerr.labajo@cit.edu>
2023-08-02 21:17:46 +0800

    Format analyze_image for readability in Utility

--- src/helpers/Utility.py
+++ src/helpers/Utility.py
@@ -24,15 +24,20 @@ def sendResponse(result):
 def analyze_image(filename):
     from app import app
     model = YOLO('src\\pre-trained_weights\\yolov8s\\lsc_v1.pt')
+    
     file_path = os.path.join('src\\uploads\\', filename)
+    
     now = datetime.now()
     date_time_str = now.strftime('%Y-%m-%d_%H-%M')
-    result = model.predict(source=file_path, show=False, conf=0.20, project="src/predictions", name=date_time_str, save=True)
-    print("-----------------------------------------------------")
-    # analyzed_folder = "ANALYZED_FOLDER"
-    # analyzed_file_path = os.path.join(analyzed_folder, filename)
-    # os.makedirs(analyzed_folder, exist_ok=True)
-    # shutil.move(file_path, analyzed_file_path)
+    
+    result = model.predict(
+        source=file_path, 
+        show=False, 
+        conf=0.20, 
+        project="src/predictions", 
+        name=date_time_str, save=True
+    )
+    
     result[0].path = "src/predictions/" + date_time_str + "/" + filename
     
     return result
