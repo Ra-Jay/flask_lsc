8fb4e00ef00e5986c0d91270f4b2dcf1aa813eff
kerrlabajo <kerr.labajo@cit.edu>
2023-08-03 01:27:05 +0800

    Modify HomeController
    Modifications:
    1. Set upload route to /upload
    2. Import time for counting elapsed time per processes.
    3. Modify printing details with elapsed times.
    4. Add conditions for handling existing input/output files.

--- src/controllers/HomeController.py
+++ src/controllers/HomeController.py
@@ -8,11 +8,12 @@ from helpers.db import insert_input_file, get_input_file, insert_output_file, ge
 from datetime import datetime
 from helpers.Utility import analyze_image, get_file_dimensions, get_file_size, get_file_extension, allowed_file
 from flask import session
+from time import time
 
 input_file = Blueprint('input_file', __name__)
 output_file = Blueprint('output_file', __name__)
 
-@input_file.route('/', methods=['GET', 'POST'])
+@input_file.route('/upload', methods=['GET', 'POST'])
 def upload():
     from app import app
     if request.method == 'POST':
@@ -28,27 +29,48 @@ def upload():
             return redirect(request.url)
 
         print("===============================")
+        start_time = time()  # Record the start time
+        
         filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-
+        path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(path)
+        
+        elapsed_time = time() - start_time  # Calculate elapsed time
+        print(f"File saved in {elapsed_time:.2f} seconds")
+        start_time = time()  # Reset start time for the next step
+        
         dimensions = get_file_dimensions(file)
-        size = get_file_size(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        size = get_file_size(path)
         extension = get_file_extension(filename)
 
+        elapsed_time = time() - start_time  # Calculate elapsed time
+        print(f"File dimensions, size, and extension calculated in {elapsed_time:.2f} seconds")
+        start_time = time()  # Reset start time for the next step
+        
         input_file = InputFile(dimensions, size, extension, filename)
         session['input_file'] = {
             'dimensions': dimensions,
             'size': size,
             'extension': extension,
-            'filename': filename
+            'filename': filename,
         }
         
-        # Check if weight already exists in the database
-        if get_weights('lsc_v1') is None:
-            insert_weights('src\\pre-trained_weights\\yolov8s\\lsc_v1.pt', 'lsc_v1')
+        # Check if input_file doesn't exist, then insert it. 
+        # If it exists and its data is the same, return the existing input_file.
+        # If it exists and its data is different, insert the new input_file.
+        existing_input_file = get_input_file(filename)
+        if existing_input_file is None:
+            insert_input_file(path)
+        else:
+            if existing_input_file['dimensions'] != dimensions or existing_input_file['size'] != size or existing_input_file['extension'] != extension:
+                insert_input_file(path)
+            else:
+                input_file = existing_input_file
         
-        insert_input_file(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-
+        elapsed_time = time() - start_time  # Calculate elapsed time
+        print(f"Input file object created and inserted in {elapsed_time:.2f} seconds")
+        print("===============================")
+        
         return render_template('index.html', input_file=input_file)
 
     return render_template('index.html')
@@ -66,15 +88,31 @@ def analyze():
         filename = input_file_data['filename']
         
         # Predict the image using the trained YOLO model
-        print(filename)
+        print("===============================")
+        print("Analyzing " + filename)
+        start_time = time() 
+        
         result = analyze_image(filename)
-        print("result: ---------------------------------")
-        print(result[0].boxes)
+        
+        elapsed_time = time() - start_time
+        print("\n===============================")
+        print(f"Image analyzed in {elapsed_time:.2f} seconds")
+        
         path = result[0].path
+        print("Image saved to " + path)
+        print("===============================")
+        start_time = time()
         
         classification = "No Good" if torch.tensor(result[0].boxes.cls).item() > 0 else "Good"
         accuracy = round(torch.tensor(result[0].boxes.conf).item(),2 )
         error_rate = round(1 - accuracy, 2)
+        
+        elapsed_time = time() - start_time
+        print("===============================")
+        print(f"Classification, accuracy, and error rate calculated in {elapsed_time:.2f} seconds")
+        print("===============================")
+        
+        start_time = time()
         
         output_file = OutputFile(classification, accuracy, error_rate, path, filename)
         session['output_file'] = {
@@ -85,7 +123,21 @@ def analyze():
             'filename': filename
         }
         
-        insert_output_file(path)
+        # Check if output_file doesn't exist, then insert it.
+        # If it exists and its data is the same, return the existing output_file.
+        # If it exists and its data is different, insert the new output_file.
+        existing_output_file = get_output_file(filename)
+        if existing_output_file is None:
+            insert_output_file(path)
+        else:
+            if existing_output_file['classification'] != classification or existing_output_file['accuracy'] != accuracy or existing_output_file['error_rate'] != error_rate:
+                insert_output_file(path)
+            else:
+                output_file = existing_output_file
+        
+        elapsed_time = time() - start_time
+        print(f"Output file object created and inserted in {elapsed_time:.2f} seconds")
+        print("===============================")
         
         return render_template('index.html', input_file=input_file_data, output_file=output_file)
     
