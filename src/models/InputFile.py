b892c9ec6a51611dfcf037b7c7f543768a74e323
kerrlabajo <kerr.labajo@cit.edu>
2023-08-04 01:19:44 +0800

    Add lacking properties for input and output file models

--- src/models/InputFile.py
+++ src/models/InputFile.py
@@ -1,6 +1,11 @@
 class InputFile:
-    def __init__(self, dimensions, size, extension, filename):
+    def __init__(self, name, dimensions, size, extension, data, weight_id, created_at, updated_at, deleted_at):
+        self.name = name
         self.dimensions = dimensions
         self.size = size
         self.extension = extension
-        self.filename = filename
+        self.data = data
+        self.weight_id = weight_id
+        self.created_at = created_at
+        self.updated_at = updated_at
+        self.deleted_at = deleted_at
