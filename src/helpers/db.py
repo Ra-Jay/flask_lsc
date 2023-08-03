c3de0a4464f74ac48c3b6ffcd96bbc91a8aca38a
kerrlabajo <kerr.labajo@cit.edu>
2023-08-04 01:25:42 +0800

    Modify db.py
    Modifications:
    1. Remove parameters for all file insertions to mongodb.
    2. Made full use of sessions instead of parameters.
    3. Remove long comment.

--- src/helpers/db.py
+++ src/helpers/db.py
@@ -6,78 +6,57 @@ import io
 from PIL import Image
 from flask import session
 
-def insert_input_file(input_image_path):
+def insert_input_file():
     mongo = MongoClient(MONGO_URI)[MONGO_DB]  # Connect to MongoDB using the URI and database name
-    with open(input_image_path, "rb") as image_file:
-        input_image = image_file.read()
-    
     current_input_file = session.get('input_file')
     
     image_document = {
         "_id": mongo[MONGO_COLLECTION_INPUT_FILES].count_documents({}) + 1,
-        "name": current_input_file['filename'], 
+        "name": current_input_file['name'], 
         "dimensions": current_input_file['dimensions'], 
         "size": current_input_file['size'], 
         "extension": current_input_file['extension'], 
-        "data": Binary(input_image),
-        "weight_id": get_weights('lsc_v1')['_id'],
-        "created_at": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-        "updated_at": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-        "deleted_at": None
+        "data": current_input_file['data'],
+        "weight_id": current_input_file['weight_id'],
+        "created_at": current_input_file['created_at'],
+        "updated_at": current_input_file['updated_at'],
+        "deleted_at": current_input_file['deleted_at']
     }
+    
     mongo[MONGO_COLLECTION_INPUT_FILES].insert_one(image_document)
     
 def get_input_file(image_name):
     mongo = MongoClient(MONGO_URI)[MONGO_DB]
     return mongo[MONGO_COLLECTION_INPUT_FILES].find_one({"name": image_name})
-    
-    # Save the image to a file
-    # Not yet necessary saving the file locally, but for when history page is implemented
-    # Can either be saving or returning a value/image to be displayed in the frontend
-    # If saving, uncomment the following line but make sure to create the folder first
-    # binary_data = document["data"]
-    # stream = io.BytesIO(binary_data)
-    # image = Image.open(stream)
-    # image.save('src/mongodb/input_images/' + image_name)
 
-def insert_output_file(predicted_image_path):
+def insert_output_file():
     mongo = MongoClient(MONGO_URI)[MONGO_DB]  # Connect to MongoDB using the URI and database name
-    with open(predicted_image_path, "rb") as image_file:
-        predicted_image = image_file.read()
     
     current_output_file = session.get('output_file')
     
     # Add input id to output file
     image_document = {
         "_id": mongo[MONGO_COLLECTION_OUTPUT_FILES].count_documents({}) + 1,
-        "name": current_output_file['filename'],
+        "name": current_output_file['name'],
         "classification": current_output_file['classification'],
         "accuracy": current_output_file['accuracy'],
         "error_rate": current_output_file['error_rate'],
-        "data": Binary(predicted_image),
-        "input_id": get_input_file(current_output_file['filename'])['_id'],
-        "created_at": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-        "updated_at": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-        "deleted_at": None
+        "data": current_output_file['data'],
+        "input_id": current_output_file['input_id'],
+        "created_at": current_output_file['created_at'],
+        "updated_at": current_output_file['updated_at'],
+        "deleted_at": current_output_file['deleted_at']
     }
     mongo[MONGO_COLLECTION_OUTPUT_FILES].insert_one(image_document)
 
-# This method was for testing purposes to decode the binary data and save/display as an image.
 def get_output_file(image_name):
     mongo = MongoClient(MONGO_URI)[MONGO_DB]
     return mongo[MONGO_COLLECTION_OUTPUT_FILES].find_one({"name": image_name})
     
-    # Save the image to a file
-    # Not yet necessary saving the file locally, but for when history page is implemented
-    # Can either be saving or returning a value/image to be displayed in the frontend
-    # If saving, uncomment the following line but make sure to create the folder first
-    # binary_data = document["data"]
-    # stream = io.BytesIO(binary_data)
-    # image = Image.open(stream)
-    # image.save('src/mongodb/predicted_images/' + image_name)
-    
-def insert_weights(weights_file_path, filename):
+def insert_weights():
     mongo = MongoClient(MONGO_URI)[MONGO_DB]
+    
+    current_weights = session.get('loaded_weights')
     
     # best.pt file is too large (22mb) to be stored in MongoDB (16mb maximum), so we will store its path instead
     # Read the PyTorch model file
@@ -94,20 +73,17 @@ def insert_weights(weights_file_path, filename):
     # https://github.com/kerrlabajo/cs346-ml.net-lsc-inspector/tree/enhancement/refactor/LSC_Intellysis/runs/detect/train7/weights/best.pt
     model_document = {
         "_id": mongo[MONGO_COLLECTION_WEIGHTS].count_documents({}) + 1,
-        "name": filename,
-        "path": weights_file_path,
-        "trained_at": "2023-05-18_08-46",
-        "created_at": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-        "updated_at": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-        "deleted_at": None
+        "name": current_weights['name'],
+        "path": current_weights['path'],
+        "trained_at": current_weights['trained_at'],
+        "created_at": current_weights['created_at'],
+        "updated_at": current_weights['updated_at'],
+        "deleted_at": current_weights['deleted_at']
     }
-    mongo[MONGO_COLLECTION_WEIGHTS].insert_one(model_document)
+    
+    return mongo[MONGO_COLLECTION_WEIGHTS].insert_one(model_document)
 
 def get_weights(model_name):
     mongo = MongoClient(MONGO_URI)[MONGO_DB]
 
-    # Find the model document by name
-    model_document = mongo[MONGO_COLLECTION_WEIGHTS].find_one({"name": model_name})
-    
-    # Return the model document
-    return model_document
+    return mongo[MONGO_COLLECTION_WEIGHTS].find_one({"name": model_name})
